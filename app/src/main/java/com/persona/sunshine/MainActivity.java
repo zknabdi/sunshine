d0b0399477c55a1eb2d7b7f2394153b2f93cd0ab
Zakaria <zknabdi@gmail.com>
2019-04-17 00:54:35 -0500

    Adding Basics of the weather app

--- app/src/main/java/com/persona/sunshine/MainActivity.java
+++ app/src/main/java/com/persona/sunshine/MainActivity.java
@@ -2,12 +2,40 @@ package com.persona.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // COMPLETED (1) Create a field to store the weather display TextView
+    private TextView mWeatherTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = (TextView)findViewById(R.id.tv_weather_data);
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+        String [] fakeWeatherData ={
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+        };
+
+        // TODO (4) Append each String from the fake weather data array to the TextView
+        for(String weatherDay: fakeWeatherData){
+            mWeatherTextView.append(weatherDay+"\n\n\n");
+        }
     }
 }
